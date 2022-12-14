aebbbf128cc56a190024fb5d62fa62b276cecc28
Umair Rehman <21343766@stu.mmu.ac.uk>
2022-12-14 19:21:13 +0000

    init

--- Multisync/src/App.tsx
+++ Multisync/src/App.tsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return( 
+  
+  <div className="bg-slate-100">
+
+    <p className="">hello world</p>
+    <p >hello world</p>
+    <p>test world</p>
+
+  </div>
+
+  )
 }
 
 export default App;
---
+++ Multisync/src/Login.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function Login() {
+  return( 
+  
+  <div className=" flex flex-col space-y-3">
+
+    <div className="bg-gray-200 w-full h-32">
+        
+    </div>
+    <div className="bg-gray-200 w-80">body</div>
+
+  </div>
+
+  )
+}
+
+export default Login;
