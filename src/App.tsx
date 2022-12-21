3837508fe8b1346353142069f085285b7326e4cc
GergoN02 <nemethgergo02@gmail.com>
2022-12-21 18:40:56 +0000

    refactor: refactor of folder structure, adds workflow and QoL improvements

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import type { ReactElement } from "react";
+import Login from "./Components/Login/Login";
+
+const App = (): ReactElement => (
+  <div className="App">
+    <Login />
+  </div>
+);
+
+export default App;
---
+++ src/Components/Login/Login.tsx
@@ -0,0 +1,10 @@
+import type { ReactElement } from "react";
+
+const Login = (): ReactElement => (
+  <div className=" flex flex-col space-y-3">
+    <div className="h-32 w-full bg-gray-200" />
+    <div className="w-80 bg-gray-200">body</div>
+  </div>
+);
+
+export default Login;
