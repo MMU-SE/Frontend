66a240c463ba08c216fe635e083964f2106abeb2
Muhammad Abdullah <87866666+MA1002643@users.noreply.github.com>
2022-12-26 22:53:48 +0000

    Sign up code start off
    To my understandings I have completed the signup page but for some reasons the code is not working I am not sure if it is my laptop messing up or the code itself in not working. 
    
    Can you please check what's the reason behind of the code not working @ntflix @GergoN02?

--- src/Components/Signup/Signup.tsx
+++ src/Components/Signup/Signup.tsx
@@ -1,1 +1,22 @@
-export {};
+import { Singup } from "react";
+
+export const SingUp = (callback: any, initialState = {}) => {
+    const [values, setValues] = Singup(initialState);
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
+    };
+}
+
+    const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        await callback();
+    };
+
+    return {
+        onChange,
+        onSubmit,
+        values,
+    };
+
+export default Signup;
