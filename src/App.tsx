bf7e43aef5dbde7df02cd302b20470bae56b9720
gergon02 <nemethgergo02@gmail.com>
2023-02-17 00:19:15 +0000

    chore: adds ReactLocation, some placeholder routes and updates Readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import type { ReactElement } from "react";
+import { Outlet, ReactLocation, Router } from '@tanstack/react-location'
+import type { ReactElement } from 'react'
+import { ReactLocationDevtools } from '@tanstack/react-location-devtools'
+import routes from 'routes'
+
+const reactLocation = new ReactLocation()
 
 const App = (): ReactElement => (
-  <div>
-    <h1 className="bg-blue-900 text-red-500">Hello World</h1>
-  </div>
-);
+	<Router location={reactLocation} routes={routes}>
+		<Outlet />
+		{import.meta.env.DEV ? (
+			<div id='devtools'>
+				<ReactLocationDevtools initialIsOpen={false} />
+			</div>
+		) : undefined}
+	</Router>
+)
 
-export default App;
+export default App
