61d42bee6f7147fc6ebacca416b985fa1ba96b28
gergon02 <nemethgergo02@gmail.com>
2023-03-15 01:57:04 +0000

    fix: removes api call from unecessary page

--- src/pages/SomePage/SomePage.tsx
+++ src/pages/SomePage/SomePage.tsx
@@ -1,28 +1,9 @@
-import useProductsQuery from 'hooks/queries/useProductsQuery'
 import type { ReactElement } from 'react'
 
-const SomePage = (): ReactElement => {
-	const {
-		data: productsData,
-		isFetching,
-		isError
-	} = useProductsQuery({ limit: 10 })
-
-	if (!isFetching && isError) {
-		return <div>Error</div>
-	}
-
-	return (
-		<div>
-			<h1>Some Page</h1>
-			{productsData?.data.map(product => (
-				<div key={product.id}>
-					<span>{product.name}</span>
-					<span>{product.description}</span>
-				</div>
-			))}
-		</div>
-	)
-}
+const SomePage = (): ReactElement => (
+	<div>
+		<h1>Some Page</h1>
+	</div>
+)
 
 export default SomePage
